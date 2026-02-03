8f4d0c343d0255428905ac2f0c8001eb89f45c02
hosam3574 <hossamtradat@gmail.com>
2026-02-03 23:49:11 +0300

    save

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import LandingHome from "./components/Home/LandingHome.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login.jsx";
+import { Toaster } from "react-hot-toast";
+import UserDashboard from "./components/Users/UserDashboard.jsx";
+import Register from "./components/Auth/Register.jsx";
+import AdminDashboard from "./components/Admin/AdminDashboard.jsx";
+import DisplayUsers from "./components/Admin/Users/DsiplayUsers.jsx";
+import DisplayCategories from "./components/Admin/Categories/DisplayCategories.jsx";
+import DisplayProducts from "./components/Admin/Products/DisplayProducts.jsx";
+import About from "./components/Shared/About.jsx";
+import Profile from "./components/Profile/Profile.jsx";
+import ProtectRoutes from "./components/Routes/ProtectRoutes.jsx";
+import AdminRoutes from "./components/Routes/AdminRoutes.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toaster position="top-center" />
+      {/* define routes */}
+
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingHome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectRoutes />}>
+            {/* Users Routes */}
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/profile" element={<Profile />} />
+            {/* Admin Routes */}
+            <Route element={<AdminRoutes />}>
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
+              <Route
+                path="/admin/dashboard/products"
+                element={<DisplayProducts />}
+              />
+              <Route
+                path="/admin/dashboard/categories"
+                element={<DisplayCategories />}
+              />
+              <Route path="/admin/dashboard/users" element={<DisplayUsers />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
